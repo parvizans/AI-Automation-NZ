0b9a83a28ba8a2bd1d9f1f2bf571f20907d2fdb6
parvizans <117871895+parvizans@users.noreply.github.com>
2026-04-06 14:36:34 +1200

    Update timesheet.js

--- sites/timesheet.js
+++ sites/timesheet.js
@@ -33,7 +33,7 @@ function generateMonth(){
       <td><input type="date" value="${date}"></td>
       <td>${getDayName(d)}</td>
       <td><input type="time" value="08:00" readonly></td>
-      <td><input type="time" value="18:00"></td>
+      <td><input type="time"></td>
       <td class="total">0</td>
       <td class="ot">0</td>
       <td class="food">-</td>
